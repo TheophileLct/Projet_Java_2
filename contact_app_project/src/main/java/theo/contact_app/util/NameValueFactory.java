55be5974afbd303ffb42a58045dc0f9c10f44bc9
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2020-02-24 18:04:11 +0100

    NameValueFactory
    -> como el tp.

--- contact_app_project/src/main/java/theo/contact_app/util/NameValueFactory.java
+++ contact_app_project/src/main/java/theo/contact_app/util/NameValueFactory.java
@@ -1,5 +1,15 @@
 package theo.contact_app.util;
 
-public class NameValueFactory {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.util.Callback;
+import theo.contact_app.model.Contact;
 
+public class NameValueFactory implements Callback<TableColumn.CellDataFeatures<Contact, String>, ObservableValue<String>> {
+	//Il manquera un Override ici. 
+	public ObservableValue<String> call(CellDataFeatures<Contact, String> cellData) {
+		return new SimpleStringProperty(cellData.getValue().getLastname() + " " + cellData.getValue().getFirstname());
+	}
 }
