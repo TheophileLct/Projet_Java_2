04a8fe721dc253ef044627fd18adda1bdf3a8f9a
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2020-02-24 18:40:22 +0100

    ContactService

--- contact_app_project/src/main/java/theo/contact_app/service/ContactService.java
+++ contact_app_project/src/main/java/theo/contact_app/service/ContactService.java
@@ -1,5 +1,19 @@
 package theo.contact_app.service;
 
-public class ContactService {
+import java.time.LocalDate;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import theo.contact_app.model.Address;
+import theo.contact_app.model.Contact;
+
+public class ContactService {
+	private ObservableList<Contact> contacts;
+	private ObservableList<Contact> contactsDataBase; //Quand on aura fait la bdd 
+	
+	private ContactService() {
+		contactsDataBase = FXCollections.observableArrayList();
+		contacts=FXCollections.observableArrayList();
+		contacts.add(new Contact(1, "lastname1", "firstname1", "nickname1", "0665487562", new Address("", "", "address1", "", "", -1), "email_address_1", LocalDate.now()));
+	}
 }
