b9be960407fb81ba1efd1ce2c8de826ff8df3a4c
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2020-02-24 17:00:48 +0100

    Stage et view

--- contact_app_project/src/main/java/theo/contact_app/service/ViewService.java
+++ contact_app_project/src/main/java/theo/contact_app/service/ViewService.java
@@ -1,5 +1,26 @@
 package theo.contact_app.service;
 
+import java.io.IOException;
+
+import theo.contact_app.App;
+import javafx.fxml.FXMLLoader;
+
 public class ViewService {
+	public static <T> T getView(String id) {
+		return getLoader(id).getRoot();
+	}
+
+	private static FXMLLoader getLoader(String id) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(App.class.getResource("view/" + id + ".fxml"));
+			loader.load();
+			return loader;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
 
 }
