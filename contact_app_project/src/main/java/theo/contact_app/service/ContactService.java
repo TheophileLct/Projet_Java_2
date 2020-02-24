2693d8e373a844262455e0ff7288690f5dc064c4
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2020-02-24 21:22:09 +0100

    Ajout

--- contact_app_project/src/main/java/theo/contact_app/service/ContactService.java
+++ contact_app_project/src/main/java/theo/contact_app/service/ContactService.java
@@ -1,7 +1,6 @@
 package theo.contact_app.service;
 
 import java.time.LocalDate;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import theo.contact_app.model.Address;
@@ -16,4 +15,23 @@ public class ContactService {
 		contacts=FXCollections.observableArrayList();
 		contacts.add(new Contact(1, "lastname1", "firstname1", "nickname1", "0665487562", new Address("", "", "address1", "", "", -1), "email_address_1", LocalDate.now()));
 	}
+	
+	private static class ContactServiceHolder {
+		private static ContactService INSTANCE;
+	}
+	
+	public static ObservableList<Contact> getContacts() {
+		return ContactServiceHolder.INSTANCE.contacts;
+	}
+	
+	public static void addContact(Contact contact) {
+		ContactServiceHolder.INSTANCE.contactsDataBase.add(contact);
+		ContactServiceHolder.INSTANCE.contacts.add(contact);
+	}
+	
+	public static void deleteContact(Contact contact) {
+		ContactServiceHolder.INSTANCE.contactsDataBase.remove(contact);
+		ContactServiceHolder.INSTANCE.contacts.remove(contact);
+	}
+	
 }
